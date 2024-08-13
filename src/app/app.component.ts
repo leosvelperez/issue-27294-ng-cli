3ac8b1c9be18db27b42e182d86e4d38d6fcb57a9
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2024-08-13 14:29:35 +0200

    add crypto-js and set appShell option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import WordArray from 'crypto-js/lib-typedarrays';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'foo';
+
+  constructor() {
+    const someVar = WordArray.random(40);
+    this.title = 'i got this ' + someVar;
+    console.log('what is someVar', someVar);
+  }
 }
